3c83bcb8d16125390eb8bcc879f69c91c801e0df
Diego <dise4888@colorado.edu>
2023-06-15 13:01:26 -0600

    Dual Channel Input

--- SDI Labs/Python_Plotter/Test.py
+++ SDI Labs/Python_Plotter/Test.py
@@ -14,18 +14,19 @@ fig, ax = plt.subplots()
 ser = serial.Serial(com[0].device, 2000000, timeout=1)
 ser.flush()
 
-size = 500
+size = 1000
 
 def animate(i, data1, data2, ser): 
     num = 0
-    if ser.in_waiting:
+    while ser.in_waiting:
         line = ser.readline()
         string = line.decode().rstrip().split(',')
         try:
-            num = float(string)
-            print(num)
-            data1.append(num[0])
-            data2.append(num[1])
+            num1 = float(string[0])
+            num2 = float(string[1])
+            print(string[0] +  ", " + string[1])
+            data1.append(num1)
+            data2.append(num2)
         except:
             pass
     else:
@@ -37,10 +38,11 @@ def animate(i, data1, data2, ser):
     ax.plot(data1)
     ax.plot(data2)
     ax.set_xlim([0, size])
+    ax.set_ylim([0, 2000])
     ax.set_title('Plot of Multiple Data Lines From Arduino')
     ax.set_xlabel('Samples')
     ax.set_ylabel('Amplitude (mV)')
 
-ani = animation.FuncAnimation(fig, animate, fargs=(data1, data2, ser), interval = 1, cache_frame_data1=True)
+ani = animation.FuncAnimation(fig, animate, fargs=(data1, data2, ser), interval = 1)
 
 plt.show()
--- SDI Labs/SDI_Scope_Test/SDI_Scope_Test.ino
+++ SDI Labs/SDI_Scope_Test/SDI_Scope_Test.ino
@@ -1,22 +1,23 @@
 ///////scope SDI/////////////////////////
 int k = 0;
 const long maxBuffer = 1500;
-long envVars[] = { 1, 500}; // number of points to average, size of buffer
+long envVars[] = { 25, 500}; // number of points to average, size of buffer
 int pinADC0 = A1;
 int pinADC1 = A2;
 long V_DAC_ADU;
-float mV_per_ADU = 3280 / 4095.0;  // for SAMD21
+double mV_per_ADU = 3280 / 4095.0;  // for SAMD21
 //long arrV_ADU1[maxBuffer];
 //double arrV_mV1[maxBuffer];
 //long arrV_ADU2[maxBuffer];
 //double arrV_mV2[maxBuffer];
 //double V_DAC_V = 1.0;
 //////timing////////////////////////////////////
-long iBaudRate = 2000000;
+long iBaudRate = 250000;
 
 void setup() {
   Serial.begin(iBaudRate);
   delay(1000);  //lets the serial port get initialized
+  analogReadResolution(12);
   //V_DAC_ADU = V_DAC_V * 1023.0 / 3.28; // to use as a reference
   //analogWrite(A0, V_DAC_ADU);
 
@@ -29,21 +30,22 @@ void loop() {
 void runScope() {
   int ADC0_ADU = 0;
   int ADC1_ADU = 0;
-  int ADC0_mV = 0;
-  int ADC1_mV = 0;
+  double ADC0_mV = 0;
+  double ADC1_mV = 0;
   
   for (long i = 0; i < envVars[0]; i++) {
     ADC0_ADU = ADC0_ADU + analogRead(pinADC0);
     ADC1_ADU = ADC1_ADU + analogRead(pinADC1);
+    //Serial.println("Running");
   }
   ///// calc the average per point and converting to mV ////////
-  for (int j = 0; j < envVars[1]; j++) {
-    ADC0_mV = ADC0_ADU / (envVars[0] * 1.0) * mV_per_ADU;
-    ADC1_mV = ADC1_ADU / (envVars[0] * 1.0) * mV_per_ADU;
-    Serial.print(ADC0_mV);
-    Serial.print(",");
-    Serial.println(ADC1_mV);
-  }
+  //for (int j = 0; j < envVars[1]; j++) {
+  ADC0_mV = (double)ADC0_ADU / (envVars[0] * 1.0) * mV_per_ADU;
+  ADC1_mV = (double)ADC1_ADU / (envVars[0] * 1.0) * mV_per_ADU;
+  Serial.print(ADC0_mV);
+  Serial.print(",");
+  Serial.println(ADC1_mV);
+  //}
 }
 
 /*
