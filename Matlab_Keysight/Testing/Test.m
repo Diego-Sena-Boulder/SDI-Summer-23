0c9e17dd2dd7f6581ba349e1083ca14cae598946
Diego <dise4888@colorado.edu>
2023-07-07 10:12:31 -0600

    PCB and Scope Update

--- Matlab_Keysight/Testing/Test.m
+++ Matlab_Keysight/Testing/Test.m
@@ -3,22 +3,43 @@ clearvars
 close all
 clc
 
+%% Set Vars
+samplerate = 5e9;
+sampletime = 1e-6;
+sampleLen = sampletime*samplerate;
+maxTime = 1e9;
+
 %% Instrument Connection & Reset Device
-myScope = ividev('AgInfiniiVision', 'USB0::0x0957::0x17A6::MY57310896::0::INSTR'); % ResetDevice = true
+devlist = ividevlist("Timeout",40);
+myScope = ividev(devlist.MATLABDriver(1), devlist.ResourceName(1), ResetDevice = true); 
 
 %% Set Scope Up
-sampleLength = 5000;
-maxTime = 1e9;
 % Configure horizontal time scale
-myScope.Acquisition.HorizontalTimePerRecord = 100e-8; % Seconds
-
+myScope.Acquisition.HorizontalTimePerRecord = sampletime; % Seconds
+myScope.Channel("Channel1").VerticalRange = 2;
+myScope.Channel("Channel1").ProbeAttenuation = 1;
 %% Gather Data
-[waveformArray, actualPoints] = readWaveform(myScope, "Channel1", sampleLength, maxTime);
+[waveformArray, actualPoints] = readWaveform(myScope, "Channel1", sampleLen, maxTime);
 dt = myScope.Acquisition.HorizontalTimePerRecord/myScope.Acquisition.HorizontalRecordLength;
-t = (0:sampleLength-1) * dt;
+t = (0:sampleLen-1) * dt;
 
 figure (1)
-plot(t, waveformArray)
+plot(t, waveformArray, 'LineWidth', 1)
 grid on;
 xlabel('Time(s)')
 ylabel('Volts (V)')
+
+%% FFT the Data
+DataFFT = abs(fft(waveformArray) ./ sampleLen);
+f_HZ = (0:sampleLen/2-1)*(myScope.Acquisition.HorizontalSampleRate/sampleLen);
+dataFFT = DataFFT(1:sampleLen/2);
+figure (2)
+semilogy(f_HZ, dataFFT)
+grid on;
+xlabel('Frequency (hz)', 'LineWidth', 2)
+ylabel('Magnitude')
+xlim([0 1e9])
+
+
+
+
