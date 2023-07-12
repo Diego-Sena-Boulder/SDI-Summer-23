2b9d13667900e46207377f0053b5a0830e320d52
Diego <dise4888@colorado.edu>
2023-07-12 08:24:11 -0600

    30MHZ Test

--- Matlab_Keysight/Testing/Test.m
+++ Matlab_Keysight/Testing/Test.m
@@ -5,9 +5,10 @@ clc
 
 %% Set Vars
 sampleRateHz = 5e9;
-sampleTimeS = 1e-6;
+sampleTimeS = 200e-9;
 sampleLen = sampleTimeS*sampleRateHz;
 maxTime = 1e9;
+numAvg = 100;
 
 %% Instrument Connection & Reset Device
 devlist = ividevlist("Timeout",40);
@@ -17,13 +18,20 @@ myScope.Acquisition.InitiateContinuous = true;
 %% Set Scope Up
 % Configure horizontal time scale
 myScope.Acquisition.HorizontalTimePerRecord = sampleTimeS; % Seconds
-myScope.Channel("Channel1").VerticalRange = 4;
+myScope.Channel("Channel1").VerticalRange = 1.2;
 myScope.Channel("Channel1").ProbeAttenuation = 1;
-myScope.Trigger.TriggerLevel = 1;
+myScope.Trigger.TriggerLevel = 0;
 %% Gather Data
 sampleLen = myScope.Acquisition.HorizontalRecordLength;
 sampleRateHz = myScope.Acquisition.HorizontalSampleRate;
 [waveformArray, actualPoints] = readWaveform(myScope, "Channel1", sampleLen, maxTime);
+
+for n = 0:(numAvg - 2)
+    [holdwaveformArray, actualPoints] = readWaveform(myScope, "Channel1", sampleLen, maxTime);
+    waveformArray = waveformArray + holdwaveformArray;
+end
+
+waveformArray = waveformArray ./ numAvg;
 dt = myScope.Acquisition.HorizontalTimePerRecord/myScope.Acquisition.HorizontalRecordLength;
 t = (0:sampleLen-1) * dt;
 
@@ -35,16 +43,15 @@ ylabel('Volts (V)')
 
 %% FFT the Data
 %data = waveformArray - mean(waveformArray); % removed dc
-DataFFT = abs(fft(waveformArray) ./ sampleLen);
+DataFFT = fft(waveformArray, sampleLen) ./ sampleLen .* 2;
 f_HZ = (0:sampleLen/2-1)*(myScope.Acquisition.HorizontalSampleRate/sampleLen);
-dataFFT = DataFFT(1:sampleLen/2);
-figure (3)
-semilogy(f_HZ, dataFFT, 'o')
+dataFFT = abs(DataFFT(1:sampleLen/2));
+figure (2)
+loglog(f_HZ, dataFFT, 'o', 'LineWidth', 2)
 grid on;
-xlabel('Frequency (hz)', 'LineWidth', 2)
-ylabel('Magnitude')
-%xlim([0 4e3])
-
+xlabel('Frequency (hz)')
+ylabel('Magnitude (Vpp)')
+xlim([0 200e6])
 
 
 
